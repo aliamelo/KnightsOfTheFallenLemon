899754a1d058d5e50d58119a97f42c98e4313895
Bakanet <32592013+Bakanet@users.noreply.github.com>
2018-03-11 14:51:16 +0100

    rezip

--- Company & Co V2/Assets/Scripts/Common/Pool/Poolable.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class Poolable : MonoBehaviour
-{
-    public string key;
-    public bool isPooled;
-}
--- Company & Co V2/Assets/Scripts/Model/LevelData.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class LevelData : ScriptableObject
-{
-    public List<Vector3> tiles;
-}
